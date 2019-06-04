166fbfc90f44bf32ec73d6518fc63a7f48d905a3
genkilife <wangyenkai@gmail.com>
2019-06-04 13:38:18 -0700

    add binary tree source codes

---
+++ src/BasicTree.cpp
@@ -0,0 +1,28 @@
+#include "BasicTree.h"
+
+BasicTree::BasicTree(){
+}
+
+BasicTree::~BasicTree(){
+}
+
+RC BasicTree::create(){
+	return SUCCESS;
+}
+
+RC BasicTree::insert(KeyType key, ValType val){
+	return SUCCESS;
+}
+
+RC BasicTree::search(KeyType key){
+	return SUCCESS;
+}
+
+RC BasicTree::printTreeMethod(){
+	std::cout<<"Basic tree class is used!"<<std::endl;
+	return SUCCESS;
+}
+
+bool BasicTree::isEmptyTree(){
+	return true;
+}
---
+++ src/BinaryTree.cpp
@@ -0,0 +1,28 @@
+#include "BinaryTree.h"
+
+BinaryTree::BinaryTree(){
+}
+
+BinaryTree::~BinaryTree(){
+}
+
+RC BinaryTree::create(){
+	return SUCCESS;
+}
+
+RC BinaryTree::insert(KeyType key, ValType val){
+	return SUCCESS;
+}
+
+RC BinaryTree::search(KeyType key){
+	return -1;
+}
+
+RC BinaryTree::printHashMethod(){
+	std::cout<<"Binary tree class is used!"<<std::endl;
+	return SUCCESS;
+}
+
+bool BinaryTree::isEmptyHash(){
+	return true;
+}
