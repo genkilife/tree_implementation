9822d5e9fb4954a8f3aaf5f4b57e958473ec8996
genkilife <wangyenkai@gmail.com>
2019-06-05 12:18:11 -0700

    implement basic bplus node prototype

--- include/BplusNode.h
+++ include/BplusNode.h
@@ -13,7 +13,11 @@ class BplusNode{
 		~BplusNode();
 
 		std::vector< ValType > datas;
-		std::vector< BplusNode* > childPtr;
+		std::vector< BplusNode* > childPtrs;
+
+		RC findData(ValType, BplusNode* &);
+		int getDatasSize();
+		int getChildPtrsSize();
 
 		const int maxDataSize;
 };
--- src/BplusNode.cpp
+++ src/BplusNode.cpp
@@ -1,20 +1,49 @@
 #include "BplusNode.h"
+#include <cassert>
 
 BplusNode::BplusNode()
 		:maxDataSize(0)
 {
 	datas.clear();
-	childPtr.clear();
+	childPtrs.clear();
 }
 
 BplusNode::BplusNode(int maxSize)
 		:maxDataSize(maxSize)
 {
 	datas.clear();
-	childPtr.clear();
+	childPtrs.clear();
+}
+
+RC BplusNode::findData(ValType data, BplusNode* & childPtr){
+	childPtr = NULL;
+	unsigned int i;
+	for(i=0; i < datas.size(); i++){
+		if(datas[i] == data){
+			return SUCCESS;
+		} else if(datas[i] > data){
+			break;
+		}
+	}
+
+	childPtr = childPtrs[i];
+	if(childPtr == NULL){
+		assert(childPtr!=NULL);
+		return -1;
+	}
+
+	return -1;
+}
+
+int BplusNode::getDatasSize(){
+	return datas.size();
+}
+
+int BplusNode::getChildPtrsSize(){
+	return childPtrs.size();
 }
 
 BplusNode::~BplusNode(){
 	datas.clear();
-	childPtr.clear();
+	childPtrs.clear();
 }
